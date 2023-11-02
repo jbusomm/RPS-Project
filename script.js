88b2e4c5210b93166279dac55b5130bb3d55c689
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-11-02 10:48:26 +0100

    Add main functions and test

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello Worldies!")
+function getComputerChoice(min , max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+function getPlayerChoice() {
+    let playerPrompt = prompt("Rock, Paper or Scissors?")
+    let playerLowerCase = playerPrompt.toLowerCase()
+    return playerLowerCase
+}
+
+const computerSelection = getComputerChoice(1 , 3)
+const playerSelection = getPlayerChoice()
+console.log(playerSelection)
+function playRound(playerSelection , computerSelection) {
+    switch (playerSelection) {
+        case "rock":
+            return console.log("checkers")
+    }
+}
+
+playRound(playerSelection , computerSelection)
