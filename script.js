19ada9d3cbc58626228c2780e8e08803ae826d47
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-11-02 11:22:53 +0100

    Add all switch statement to the round system

--- script.js
+++ script.js
@@ -19,10 +19,33 @@ function getPlayerChoice() {
 
 const computerSelection = getComputerChoice(1 , 3)
 const playerSelection = getPlayerChoice()
+
 function playRound(playerSelection , computerSelection) {
     switch (playerSelection) {
         case "rock":
-            if (computerSelection === "paper")
+            if (computerSelection === "paper") {
+                return console.log("You Lose. Paper beats Rock.")
+            } else if (computerSelection === "scissors") {
+                return console.log("You win! Rock beats scissors.")
+            } else if (computerSelection === "rock") {
+                return console.log("It's a tie! Rocks bump each other!")
+            }
+        case "paper":
+            if (computerSelection === "rock") {
+                return console.log("You Win! Paper beats Rock.")
+            } else if (computerSelection === "scissors") {
+                return console.log("You lose. Scissors beats paper.")
+            } else if (computerSelection === "paper") {
+                return console.log("It's a tie! Papers just become one and nothing happens!")
+            }
+        case "scissors":
+            if (computerSelection === "rock") {
+                return console.log("You lose. Rock beats .")
+            } else if (computerSelection === "paper") {
+                return console.log("You Win! Scissors beats paper.")
+            } else if (computerSelection === "scissors") {
+                return console.log("It's a tie! Scissors just jam each other!")
+            }
     }
 }
 
